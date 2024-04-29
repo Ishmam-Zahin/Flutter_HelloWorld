8e73e891852fe8ce8347a065574d67e9e7a049cf
zahin <mdishmamzahin@gmail.com>
2024-04-29 21:10:52 +0600

    initial

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Center(
+    child: Text(
+        "Hi, I am\nMd. Ishmam Zahin.\nThis is my first android app built using flutter.",
+        textDirection: TextDirection.ltr),
+  ));
+}
